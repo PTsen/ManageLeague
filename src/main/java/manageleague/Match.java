c9f32542e4c4d77e8f829906d7f8c4c9fddfa789
jnshuti93 <jnshuti93@yahoo.com>
2017-11-23 15:58:54 +0100

    Editing structure

--- src/main/java/manageleague/Match.java
+++ src/main/java/manageleague/Match.java
@@ -1,6 +1,52 @@
 package manageleague;
 
+import java.util.Date;
+
 public class Match {
-    public Match() {
+
+    //private fields
+    private int score_t1;
+    private int score_t2;
+    private Team home_team;
+    private Team away_team;
+    private Date match_date;
+
+    //Constructor method
+    public Match(Team team1, Team team2, Date match_date) {
+        home_team = team1;
+        away_team = team2;
+        this.match_date = match_date;
+    }
+
+    //Accessors
+    public int getScore_t1() {
+        return score_t1;
+    }
+
+    public int getScore_t2() {
+        return score_t2;
+    }
+
+    public Team getHome_team() {
+        return home_team;
+    }
+
+    public Team getAway_team() {
+        return away_team;
+    }
+
+    public Date getMatch_date() {
+        return match_date;
+    }
+
+    //Methods
+    public void DeterminePoints(){
+
+    }
+
+    public void SetScores(int score_t1, int score_t2) {
+        this.score_t1 = score_t1;
+        this.score_t2 = score_t2;
+        DeterminePoints();
     }
 }
--- src/main/java/manageleague/Team.java
+++ src/main/java/manageleague/Team.java
@@ -1,13 +1,19 @@
 package manageleague;
 
 public class Team {
+
+    //Private fields
+    private String name;
+    private int position;
+    private int drawn;
+    private int lost;
+    private int win;
+    private int played;
+    private int goal_diff;
+    private int points;
+    
     public Team() {
-        String name;
-        int position;
-        int drawn;
-        int lost;
-        int win;
-        int played;
-        int goal_diff;
     }
+
+
 }

ff9c285dd51ca084154ea55fa4e3d0234d30ea91
Steven GAROFALO <16260@ecam.be>
2017-12-01 15:00:17 +0100

    modifie testmatch

--- src/test/java/test/testMatch.java
+++ src/test/java/test/testMatch.java
@@ -1,4 +1,17 @@
 package test;
 
 public class testMatch {
+
+    @Test
+    void testSetScores() {
+        Match match = new Match("Arsenal", "Liverpool", "10/10/2017" );
+        match.SetScores(1,0);
+        if(match.DeterminePoints()!=2) {
+            fail("Wrong age");
+
+        }
+    }
+
+
+
 }

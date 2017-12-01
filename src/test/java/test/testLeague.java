fbf70148fed4a2534cdeaf2f268f816f8889ccda
jnshuti93 <jnshuti93@yahoo.com>
2017-12-01 17:37:01 +0100

    Editing League tests

--- src/test/java/test/testLeague.java
+++ src/test/java/test/testLeague.java
@@ -1,7 +1,11 @@
 package test;
 
 import static org.junit.Assert.*;
-//import org.junit.Ignore;
+import static org.hamcrest.CoreMatchers.*;
+//import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+//import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+//import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import manageleague.Match;
 import manageleague.Team;
@@ -16,28 +20,43 @@ public class testLeague {
     }
 
     /**
-     * Test method for {@link manageleague.League#AddTeams(Team)}}.
+     * Test Constructor method for {@link manageleague.League#League(String, Date, Date)}.
+     */
+    @Test
+    public void testLeagueConstruct() {
+
+        String team_name = "English Premier League";
+        Date date_start = new Date(2017,8,26);
+        Date date_end = new Date(2018,5,26);
+        manageleague.League englishLeague = new manageleague.League("English Premier League", date_start, date_end);
+
+        assertTrue("No name for League", englishLeague.getName() == team_name);
+        assertTrue("No start date for league", englishLeague.getEnd_date() == date_end);
+        assertTrue("No end date for league", englishLeague.getStart_date() == date_start);
+    }
+
+    /**
+     * Test method for {@link manageleague.League#addTeams(String)}.
      */
     @Test
     public void testAddTeams() {
 
         int result = 2;
-        String team_name = "Manchester United";
-        //Test if a team has been added to list
+        String team_name = "Arsenal";
         Date date_start = new Date(2017,8,26);
         Date date_end = new Date(2018,5,26);
         manageleague.League englishLeague = new manageleague.League("English Premier League", date_start, date_end);
-        Team arsenal = new Team("Arsenal");
-        Team manu = new Team("Manchester United");
-        englishLeague.AddTeams(arsenal);  //Call test method
-        englishLeague.AddTeams(manu);
+        Team arsenal = new Team(team_name);
+        //Call test method. Test if a team has been added to list
+        englishLeague.addTeams(team_name);
+        englishLeague.addTeams("Manchester United");
 
-        assertTrue("No team was added", englishLeague.getList_of_teams().size() == result);
-        assertTrue("No name for team", englishLeague.getList_of_teams().get(1).equals(team_name));
+        //assertThat(englishLeague.getList_of_teams(), hasItems(arsenal));
+        //assertTrue("No team was added", englishLeague.getList_of_teams().size() == result);
     }
 
     /**
-     * Test method for {@link manageleague.League#AddTeams(Team)}}.
+     * Test method for {@link manageleague.League#addMatch(Match)}.
      */
     @Test
     public void testAddMatch() {
@@ -51,7 +70,8 @@ public class testLeague {
         manageleague.League englishLeague = new manageleague.League("English Premier League", date_start, date_end);
         Team arsenal = new Team("Arsenal");
         Team manu = new Team("Manchester United");
-        englishLeague.AddTeams(arsenal);  //Call test method
+        englishLeague.addTeams("Arsenal");  //Call test method
+        englishLeague.addTeams("Manchester United");  //Call test method
         Match match1 = new Match(manu, arsenal, match_date);
 
         assertTrue("No match was added", englishLeague.getList_of_teams().size() == result);
--- src/test/java/test/testMatch.java
+++ src/test/java/test/testMatch.java
@@ -1,17 +1,15 @@
 package test;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class testMatch {
 
     @Test
     void testSetScores() {
-        Match match = new Match("Arsenal", "Liverpool", "10/10/2017" );
+       /* Match match = new Match("Arsenal", "Liverpool", "10/10/2017" );
         match.SetScores(1,0);
         if(match.DeterminePoints()!=) {
-            fail("Wrong score");
+            fail("Wrong score");*/
 
         }
-    }
-
-
-
 }

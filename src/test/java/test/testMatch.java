f4eea4ec5e107db4dc2185e91b6c983871123495
Steven GAROFALO <16260@ecam.be>
2017-12-05 16:06:09 +0100

    k

--- src/test/java/test/testMatch.java
+++ src/test/java/test/testMatch.java
@@ -8,6 +8,7 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import manageleague.League;
+import manageleague.Team;
 import org.junit.Test;
 
 import java.text.DateFormat;
@@ -19,10 +20,25 @@ public class testMatch {
 
     @Test
     void testSetScores() {
-       manageleague.Match match = new manageleague.Match("Arsenal", "Liverpool", "10/10/2017" );
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat df1 = new SimpleDateFormat("dd/MM/yy");
+        DateFormat df2 = new SimpleDateFormat("d/M/yyyy");
+        DateFormat df3 = new SimpleDateFormat("d/M/yy");
+        String name1 = "Arsenal";
+        String name2 = "Liverpool";
+        String date = "10/10/2017";
+
+        //manageleague.Team team1 = new Team(name2);
+        //manageleague.Team team2 = new Team(name1);
+
+        manageleague.Match match = new manageleague.Match(name1, name2, date);
         match.SetScores(1,0);
-        if(match.DeterminePoints()!= ) {
-            fail("Wrong score");
+
+        assertThat("name team 1", match.getHome_team().getName(), is(name1));
+        assertThat("name team 2", match.getAway_team().getName(), is(name2));
+        assertThat("Date match", df.format(match.getMatch_date()), is(date));
 
         }
+    }
 }

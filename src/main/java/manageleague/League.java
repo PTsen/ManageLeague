4d03e4f89fc272d520c049ef28536f22d0346a2a
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2017-12-05 15:55:08 +0100

    Added reset function to League

--- src/main/java/manageleague/League.java
+++ src/main/java/manageleague/League.java
@@ -165,4 +165,13 @@ public class League implements Serializable {
         
         return converted_date;
     }
+    
+    public void reset() {
+    	name = "";
+        nbr_teams= 0;
+        start_date = new Date();   //League season
+        end_date = new Date();
+        list_of_teams.clear();
+        list_of_matches.clear();
+    }
 }

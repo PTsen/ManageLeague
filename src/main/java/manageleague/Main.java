5a48e4412158c5616c886bfe76110a8ae03bb2f7
Swolfs15297 <33083208+Swolfs15297@users.noreply.github.com>
2017-12-15 16:17:16 +0100

    Test de la class Display score avec le match test du main

--- src/main/java/manageleague/Main.java
+++ src/main/java/manageleague/Main.java
@@ -19,7 +19,9 @@ public class Main {
 		Date tst = test.convertToDate("01/01/2001");
 		System.out.println(tst);
 		DisplayScore ds = new DisplayScore();
-		//ds.ScanScore();  a completer quand les matchs sont crée.
+		//Appel le match test pour modifier son score.
+		ds.ScanScore(League.getInstance().getList_of_matches().get(0));
+
 	}
 
 }
